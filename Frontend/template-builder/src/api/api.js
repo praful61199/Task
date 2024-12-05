c7c8f9d4b7c1aa05d537b4f2d26f1fa5721ce01c
praful61199 <110277893+praful61199@users.noreply.github.com>
2024-12-05 23:40:23 +0530

    Update api.js

--- Frontend/template-builder/src/api/api.js
+++ Frontend/template-builder/src/api/api.js
@@ -1,15 +1,15 @@
 export const getTemplates = async () => {
-    const response = await fetch('http://localhost:5000/api/templates');
+    const response = await fetch('https://task-backend-1wc4.onrender.com/api/templates');
     return await response.json();
   };
   
   export const getTemplate = async (id) => {
-    const response = await fetch(`http://localhost:5000/api/templates/${id}`);
+    const response = await fetch(`https://task-backend-1wc4.onrender.com/api/templates/${id}`);
     return await response.json();
   };
   
   export const saveTemplate = async (template) => {
-    const response = await fetch('http://localhost:5000/api/templates', {
+    const response = await fetch('https://task-backend-1wc4.onrender.com/api/templates', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
